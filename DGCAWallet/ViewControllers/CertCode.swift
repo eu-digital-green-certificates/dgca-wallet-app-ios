5075bb8f11e93873fa26de83622d4d70be2f540b
Yannick Spreen <yannick.spreen@gmail.com>
2021-04-30 20:32:53 +0200

    Add code prefetching.

--- DGCAWallet/ViewControllers/CertCode.swift
+++ DGCAWallet/ViewControllers/CertCode.swift
@@ -38,21 +38,6 @@ class CertCodeVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    imageView.image = generateQRCode(from: hCert.payloadString)
+    imageView.image = hCert.qrCode
   }
-}
-
-func generateQRCode(from string: String) -> UIImage? {
-  let data = string.data(using: String.Encoding.ascii)
-
-  if let filter = CIFilter(name: "CIQRCodeGenerator") {
-    filter.setValue(data, forKey: "inputMessage")
-    let transform = CGAffineTransform(scaleX: 3, y: 3)
-
-    if let output = filter.outputImage?.transformed(by: transform) {
-      return UIImage(ciImage: output)
-    }
-  }
-
-  return nil
 }
