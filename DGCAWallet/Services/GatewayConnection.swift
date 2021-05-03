5f4b402b5d99df17a2df738fd739c5f3eb36148e
Yannick Spreen <yannick.spreen@gmail.com>
2021-05-03 22:26:32 +0200

    WIP add param.

--- DGCAWallet/Services/GatewayConnection.swift
+++ DGCAWallet/Services/GatewayConnection.swift
@@ -35,7 +35,18 @@ struct GatewayConnection {
   static let claimEndpoint = "dgci/wallet/claim"
 
   public static func claim(cert: HCert, with tan: String?, completion: ((Bool) -> Void)?) {
-    AF.request(serverURI + claimEndpoint, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).response {
+    guard let tan = tan, !tan.isEmpty else {
+      completion?(false)
+      return
+    }
+    let param: [String: Any] = [
+      "dgci": cert.uvci,
+      "tanHash": SHA256.stringDigest(input: Data(tan.encode())),
+      "certHash": cert.certHash,
+      "pubKey": "TODO: DER encoded cert.keyPair pubkey",
+      "signature": "TODO: signature",
+    ]
+    AF.request(serverURI + claimEndpoint, method: .get, parameters: param, encoding: JSONEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).response {
       guard
         case .success(_) = $0.result,
         let status = $0.response?.statusCode,
