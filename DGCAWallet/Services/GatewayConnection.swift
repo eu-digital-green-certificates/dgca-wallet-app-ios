16c8b648a1c7ec8359ac55332dc45b5f966aa922
Yannick Spreen <yannick.spreen@gmail.com>
2021-05-05 14:48:48 +0200

    Disable mocking of claim response.

--- DGCAWallet/Services/GatewayConnection.swift
+++ DGCAWallet/Services/GatewayConnection.swift
@@ -71,7 +71,7 @@ struct GatewayConnection {
           let status = $0.response?.statusCode,
           status == 204
         else {
-          completion?(true)
+          completion?(false)
           return
         }
         completion?(true)
