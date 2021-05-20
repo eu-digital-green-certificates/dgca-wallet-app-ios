2a6338ce6d1146d602252f5e33a8e99b9cefdf6f
Yannick Spreen <yannick.spreen@gmail.com>
2021-05-20 22:21:53 +0200

    Change to light status bar.
    #53

--- DGCAWallet/ViewControllers/List.swift
+++ DGCAWallet/ViewControllers/List.swift
@@ -40,6 +40,10 @@ class ListVC: UIViewController {
     }
   }
 
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
