c9192914fd75cf61f31b497f3a6f025ee3ed6485
Alexandr Chernyy <pingus.nikalex@gmail.com>
2021-07-05 12:43:00 +0300

    fixed issue 82 SecureStorage: Missing argument for parameter 'fileName' in call

--- DGCAWallet/Models/LocalData.swift
+++ DGCAWallet/Models/LocalData.swift
@@ -39,7 +39,14 @@ struct DatedCertString: Codable {
 }
 
 struct LocalData: Codable {
-  static let appVersion = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? "?.?.?"
+  private enum Constants {
+    static let pubKeysStorageFilename = "secure"
+    static let bundleVersion = "CFBundleShortVersionString"
+    static let unknown = "?.?.?"
+    static let versions = "versions"
+    static let defaultVer = "default"
+  }
+  static let appVersion = (Bundle.main.infoDictionary?[Constants.bundleVersion] as? String) ?? Constants.unknown
   static var sharedInstance = LocalData()
 
   var certStrings = [DatedCertString]()
@@ -55,7 +62,7 @@ struct LocalData: Codable {
     sharedInstance.save()
   }
 
-  static let storage = SecureStorage<LocalData>()
+  static let storage = SecureStorage<LocalData>(fileName: Constants.pubKeysStorageFilename)
 
   static func initialize(completion: @escaping () -> Void) {
     storage.loadOverride(fallback: LocalData.sharedInstance) { success in
@@ -74,9 +81,9 @@ struct LocalData: Codable {
   }
 
   var versionedConfig: JSON {
-    if config["versions"][Self.appVersion].exists() {
-      return config["versions"][Self.appVersion]
+    if config[Constants.versions][Self.appVersion].exists() {
+      return config[Constants.versions][Self.appVersion]
     }
-    return config["versions"]["default"]
+    return config[Constants.versions][Constants.defaultVer]
   }
 }
