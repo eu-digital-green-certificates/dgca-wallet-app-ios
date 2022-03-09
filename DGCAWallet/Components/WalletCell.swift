501468c207fe4e9ab2d51a95f600c880895ac40a
Paul Ballmann <paul.ballmann@telekom.de>
2022-03-09 17:15:48 +0100

    feat: revocation fix

--- DGCAWallet/Components/WalletCell.swift
+++ DGCAWallet/Components/WalletCell.swift
@@ -38,10 +38,14 @@ class WalletCell: UITableViewCell {
 		typeLabel.text = cert.certTypeString
 		nameLabel.text = cert.fullName
 		dateLabel.text = String(format: "Scanned %@".localized, dated.date.localDateString)
-		guard let cert = dated.cert else { return }
+		// guard let cert = dated.cert else { return }
 		if let isRevoked = cert.isRevoked {
-			revocationLabel.isHidden = !isRevoked
+			if isRevoked {
+				revocationLabel.isHidden = false
+				revocationLabel.text = "Certificate revoked"
+			} else {
+				revocationLabel.isHidden = true
+			}
 		}
-		revocationLabel.text = "Certificate revoked"
 	}
 }
