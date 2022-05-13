427b9bfb2bf58336c00227540a063eaa921d285b
IgorKhomiak <ihorkhomyak@gmail.com>
2022-05-14 00:32:57 +0300

    added dark mode

--- DGCAWallet/SupportingFiles/SceneDelegate.swift
+++ DGCAWallet/SupportingFiles/SceneDelegate.swift
@@ -28,7 +28,13 @@ import UIKit
 
 @available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    var window: UIWindow?
+    var window: UIWindow? {
+        didSet {
+            let style = UITraitCollection.current.userInterfaceStyle
+            window?.overrideUserInterfaceStyle = style
+        }
+    }
+    
     var isNFCFunctionality = false
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
--- DGCAWallet/ViewControllers/CertificateViewer/CertPagesController.swift
+++ DGCAWallet/ViewControllers/CertificateViewer/CertPagesController.swift
@@ -47,7 +47,7 @@ class CertPagesController: UIPageViewController {
         setViewControllers([vcs[index]], direction: .forward, animated: false)
         let appearance = UIPageControl.appearance(whenContainedInInstancesOf: [UIPageViewController.self])
         appearance.pageIndicatorTintColor = UIColor.disabledText
-        appearance.currentPageIndicatorTintColor = UIColor.walletBlue
+        appearance.currentPageIndicatorTintColor = UIColor.certificateBlue
     }
 
     override func viewDidAppear(_ animated: Bool) {
